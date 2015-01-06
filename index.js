6aef1434e4889f6166b492c92870d7e816679a4e
gwiant <gwiant@me.com>
2015-01-06 11:07:40 -0800

    moving to shared require config

--- index.js
+++ index.js
@@ -1,33 +1,3 @@
-// For any third party dependencies, like jQuery, place them in the lib folder.
-
-// Configure loading modules from the lib directory,
-// except for 'app' ones, which are in a sibling
-// directory.
-requirejs.config({
-    baseUrl: './',
-	map: {
-        '*': {
-            text: 'require-text/text',
-            less: 'require-less/less',
-            json: 'require-json/json',
-            css: 'require-css/css'
-        }
-    },
-    paths: {
-    	app: 'src',
-        'require-less': 'bower_components/require-less',
-        'require-text': 'bower_components/requirejs-text',
-        'require-json': 'bower_components/requirejs-json',
-        'require-css': 'bower_components/require-css'
-    },
-    packages: [],
-    less: {
-        logLevel: 1,
-        globalVars: {}
-    },
-    shim: {}
-});
-
 // Start loading the main app file. Put all of
 // your application logic in there.
-requirejs(['app/js/helpers', 'app/js/AppController']);
+requirejs(['config/require.conf.js', 'src/js/helpers', 'src/js/AppController']);

b8c44ce7368881306f5c0a2f8646a98bbeba5a4f
gwiant <gwiant@me.com>
2015-01-06 10:57:48 -0800

    migrating to shared require config

--- config/require.conf.js
+++ config/require.conf.js
@@ -2,13 +2,17 @@
 require.config({
     map: {
         '*': {
-            text: 'requirejs-text',
-            less: 'require-less/less'
+            text: 'require-text/text',
+            less: 'require-less/less',
+            json: 'require-json/json',
+            css: 'require-css/css'
         }
     },
     paths: {
         'require-less': 'bower_components/require-less',
-        'require-text': 'bower_components/requirejs-text'
+        'require-text': 'bower_components/requirejs-text',
+        'require-json': 'bower_components/requirejs-json',
+        'require-css': 'bower_components/require-css'
     },
     packages: [],
     less: {
